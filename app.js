5acd936bfa4223eeb70746505d336721d2a58c94
aurix16 <dawn.idoko@gmail.com>
2021-07-15 22:35:26 -0500

    ...

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-// Initial Code to Serve Static Content
+// // Initial Code to Serve Static Content
 
-var express = require('express');
-var path = require('path');
-var app = express();
+// var express = require('express');
+// var path = require('path');
+// var app = express();
 
-const MongoClient = require('mongodb').MongoClient;
-const dotenv = require('dotenv')
-dotenv.config()
+// const MongoClient = require('mongodb').MongoClient;
+// const dotenv = require('dotenv')
+// dotenv.config()
 
-async function main(){
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgsz2.mongodb.net/<trial>?retryWrites=true&w=majority`;
-  console.log(`${process.env.DB_USER}`)
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// async function main(){
+//   /**
+//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//    */
+//   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgsz2.mongodb.net/<trial>?retryWrites=true&w=majority`;
+//   console.log(`${process.env.DB_USER}`)
+//   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  // try {
-  //     // Connect to the MongoDB cluster
-  //     dbConn = client.connect(); 
-  //     await client.connect();
+//   // try {
+//   //     // Connect to the MongoDB cluster
+//   //     dbConn = client.connect(); 
+//   //     await client.connect();
 
-  //     // Make the appropriate DB calls
-  //     await  listDatabases(client);
+//   //     // Make the appropriate DB calls
+//   //     await  listDatabases(client);
 
-  //     // Store into DB
-  //     app.get('/pages/', function (req, res) {
-  //       // I don't think this function is running; It isn't
-  //       console.log(req.body)
-  //       dbConn.then(function(db) {
-  //           delete req.body._id; // for safety reasons
-  //           console.log(req.body.email)
-  //           db.collection('quotesMania.quotesUsers').insertOne(req.body);
-  //       });    
-  //       res.send('Data received:\n' + JSON.stringify(req.body));
-  //   });
+//   //     // Store into DB
+//   //     app.get('/pages/', function (req, res) {
+//   //       // I don't think this function is running; It isn't
+//   //       console.log(req.body)
+//   //       dbConn.then(function(db) {
+//   //           delete req.body._id; // for safety reasons
+//   //           console.log(req.body.email)
+//   //           db.collection('quotesMania.quotesUsers').insertOne(req.body);
+//   //       });    
+//   //       res.send('Data received:\n' + JSON.stringify(req.body));
+//   //   });
 
-  // } catch (e) {
-  //     console.error(e);
-  // } finally {
-  //     await client.close();
-  // }
-}
+//   // } catch (e) {
+//   //     console.error(e);
+//   // } finally {
+//   //     await client.close();
+//   // }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-
+// async function listDatabases(client){
+//   databasesList = await client.db().admin().listDatabases();
+//   console.log("Databases:");
+//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
 
 
-app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.listen(process.env.PORT || 5500, process.env.IP || '127.0.0.1' );
+
+// app.use(express.static(path.resolve(__dirname, 'public')));
+
+// app.listen(process.env.PORT || 5500, process.env.IP || '127.0.0.1' );
 
