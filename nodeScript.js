c956f0aee7694feaa59d1c0e006c84040c10914c
aurix16 <dawn.idoko@gmail.com>
2021-07-23 22:00:04 -0500

    ...

--- nodeScript.js
+++ nodeScript.js
@@ -1,35 +1,35 @@
 
-// const MongoClient = require('mongodb').MongoClient;
-// const dotenv = require('dotenv')
-// dotenv.config()
+const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv')
+dotenv.config()
 
-// async function main(){
-//   /**
-//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-//    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-//    */
-//   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgsz2.mongodb.net/<trial>?retryWrites=true&w=majority`;
-//   console.log(`${process.env.DB_USER}`)
-//   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgsz2.mongodb.net/<trial>?retryWrites=true&w=majority`;
+  console.log(`${process.env.DB_USER}`)
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//   try {
-//       // Connect to the MongoDB cluster
-//       await client.connect();
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
 
-//       // Make the appropriate DB calls
-//       await  listDatabases(client);
+      // Make the appropriate DB calls
+      await  listDatabases(client);
 
-//   } catch (e) {
-//       console.error(e);
-//   } finally {
-//       await client.close();
-//   }
-// }
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
 
-// main().catch(console.error);
+main().catch(console.error);
 
-// async function listDatabases(client){
-//   databasesList = await client.db().admin().listDatabases();
-//   console.log("Databases:");
-//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
